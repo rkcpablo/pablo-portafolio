eea5b2e28a8cd1fdb1bc0a442cd1875ca85b66b8
Pablo Khouri <rkcpablo@gmail.com>
2026-04-08 15:59:04 -0400

    Redesigned portfolio homepage

--- app/page.tsx
+++ app/page.tsx
@@ -86,6 +86,7 @@ export default function Home() {
                 "ETL Pipelines",
                 "Statistical Analysis",
                 "Predictive Modeling",
+                "Artificial Intelligence",
                 "Power BI",
                 "Tableau",
                 "Qlik Sense",
